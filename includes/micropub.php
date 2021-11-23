48840fa7406f9413f307cab1a53709df374ce530
jeremyfelt <jeremy.felt@gmail.com>
2021-11-23 12:04:24 -0800

    Make "like" post type filtering less specific

--- includes/micropub.php
+++ includes/micropub.php
@@ -2,16 +2,16 @@
 
 namespace JIC\Micropub;
 
-add_filter( 'micropub_post_type', __NAMESPACE__ . '\filter_micropub_likes', 10, 2 );
+add_filter( 'micropub_post_type', __NAMESPACE__ . '\filter_micropub_post_type', 10, 2 );
 
 /**
- * Filter the post type for a like published with a micropub client.
+ * Filter the post type for content published with a micropub client.
  *
  * @param string $post_type The post type.
  * @param array  $input     An array of input args.
  * @return string The modified post type.
  */
-function filter_micropub_likes( $post_type, $input ) {
+function filter_micropub_post_type( $post_type, $input ) {
 	$props = mp_get( $input, 'properties' );
 
 	if ( isset( $props['like-of'] ) ) {
