2e28b5381e54af46b2fa1789b2605cab96007a8d
jeremyfelt <jeremy.felt@gmail.com>
2021-11-23 20:30:25 -0800

    Filter Shortnotes metadata to use micropub ddata instead

--- includes/micropub.php
+++ includes/micropub.php
@@ -4,6 +4,7 @@ namespace JIC\Micropub;
 
 add_filter( 'micropub_post_type', __NAMESPACE__ . '\filter_micropub_post_type', 10, 2 );
 add_filter( 'micropub_post_content', __NAMESPACE__ . '\filter_micropub_post_content', 0, 2 );
+add_filter( 'get_post_metadata', __NAMESPACE__ . '\filter_reply_to_metadata', 10, 3 );
 
 /**
  * Filter the post type for content published with a micropub client.
@@ -42,4 +43,35 @@ function filter_micropub_post_content( $content, $input ) {
 	}
 
 	return $content;
+}
+
+/**
+ * Filter Shortnotes metadata to include the stored micropub
+ * data as a reply to URL if one is available.
+ *
+ * @param null   $value No data by default.
+ * @param int    $post_id  The post ID.
+ * @param string $meta_key The meta key.
+ * @return string The reply-to URL.
+ */
+function filter_reply_to_metadata( $value, $post_id, $meta_key ) {
+	if ( 'shortnotes_reply_to_url' !== $meta_key ) {
+		return $value;
+	}
+
+	$reply_to = get_post_meta( $post_id, 'mf2_in-reply-to', true );
+
+	if ( ! $reply_to ) {
+		return $value;
+	}
+
+	if ( is_string( $reply_to ) ) {
+		return $reply_to;
+	}
+
+	if ( is_array( $reply_to ) && isset( $reply_to['url'] ) ) {
+		return $reply_to['url'];
+	}
+
+	return $value;
 }
