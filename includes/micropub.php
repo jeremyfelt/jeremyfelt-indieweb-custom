e4f654edf613ea62434bf41154ae83f26860fa5d
jeremyfelt <jeremy.felt@gmail.com>
2021-11-24 12:36:34 -0800

    Filter the reply-to name used in Shortnotes

--- includes/micropub.php
+++ includes/micropub.php
@@ -5,6 +5,7 @@ namespace JIC\Micropub;
 add_filter( 'micropub_post_type', __NAMESPACE__ . '\filter_micropub_post_type', 10, 2 );
 add_filter( 'micropub_post_content', __NAMESPACE__ . '\filter_micropub_post_content', 0, 2 );
 add_filter( 'get_post_metadata', __NAMESPACE__ . '\filter_reply_to_metadata', 10, 4 );
+add_filter( 'shortnotes_reply_to_name', __NAMESPACE__ . '\filter_shortnotes_reply_to_name', 10, 3 );
 
 /**
  * Filter the post type for content published with a micropub client.
@@ -79,4 +80,20 @@ function filter_reply_to_metadata( $value, $post_id, $meta_key, $single ) {
 	}
 
 	return $value;
+}
+
+/**
+ * Filters the text used for the reply-to name.
+ *
+ * @param string   $reply_to_name The current text.
+ * @param \WP_Post $post          A shortnote's post object.
+ * @param string   $reply_to_url  The reply-to URL.
+ * @return string The modified reply-to name.
+ */
+function filter_shortnotes_reply_to_name( $reply_to_name, $post, $reply_to_url ) {
+	if ( 'this post' === $reply_to_name ) {
+		return $reply_to_url;
+	}
+
+	return $reply_to_name;
 }
